cf5870dce4c7e9741b9b8a5a003dbc08ed1e0de5
juansantana <juansantana.analista@gmail.com>
2022-02-25 22:45:05 -0300

    Commit clone netflix

--- concatenacao.js
+++
@@ -1,5 +0,0 @@
-var nome, numero
-nome = prompt("Digite o seu nome: ")
-numero = prompt("Digite o seu numero: ")
-
-document.getElementById("paragrafo").innerText = nome + " : " + numero
--- input-output.js
+++
@@ -1,5 +0,0 @@
-var nome, idade;
-nome = prompt("Digite o seu nome:")
-idade = prompt("Digite a sua idade:")
-alert("O nome digitado foi:" + nome)
-alert("A idade digitada foi:" + idade)
